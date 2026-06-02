f667af3762b308495685a232786d96610bb8dab7
David Henao Bustamante <68203737+daveshb@users.noreply.github.com>
2026-06-02 10:36:38 -0500

    add icons

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -1,4 +1,5 @@
 import { ContextGlobal } from "@/context/Context";
+import { Moon, Sun } from "@gravity-ui/icons";
 import { Label, Switch } from "@heroui/react";
 import { useContext } from "react";
 
@@ -6,19 +7,31 @@ export const DarkMode = () => {
   const { isSelected, setIsSelected } = useContext(ContextGlobal);
 
   return (
-    <div className="p-2 border">
+    <div className="p-2 border w-40">
       <div className="text-black">Titulo</div>
 
       <Switch isSelected={isSelected} onChange={setIsSelected}>
         <Switch.Control>
-          <Switch.Thumb />
+          <Switch.Thumb>
+            <Switch.Icon>
+              {isSelected ? (
+                <Moon className="size-3 text-inherit opacity-100" />
+              ) : (
+                <Sun className="size-3 text-inherit opacity-70" />
+              )}
+            </Switch.Icon>
+          </Switch.Thumb>
         </Switch.Control>
-        <Switch.Content>
+        {/* <Switch.Content>
           <Label className="text-sm text-black">DarkMode</Label>
-        </Switch.Content>
+        </Switch.Content> */}
       </Switch>
 
-      {isSelected ? <div className="text-blue-800">Imagen de luna</div> : <div className="text-green-500">imagen de un sol</div>}
+      {isSelected ? (
+        <div className="text-blue-800">Imagen de luna</div>
+      ) : (
+        <div className="text-green-500">imagen de un sol</div>
+      )}
     </div>
   );
 };
